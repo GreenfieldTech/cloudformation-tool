ea1fdf7e517e76fcdf147dc983b291afc5d93455
tal weissler <talw@greenfieldtech.net>
2017-10-18 12:27:39 +0300

    bug fixes with the zip

--- lib/cloud_formation_tool/cloud_formation/lambda_code.rb
+++ lib/cloud_formation_tool/cloud_formation/lambda_code.rb
@@ -7,38 +7,40 @@ module CloudFormationTool
     class LambdaCode
       include Storable
       
-#      def initialize(url)
-#        log "Downloading Lambda code from #{url}"
-#        res = fetch(url)
-#        
-#        @s3_url = URI(upload(make_filename(url.split('.').last), res.body, mime_type: res['content-type'], gzip: false))
-#        log "uploaded Lambda function to #{@s3_url}"
-#      end
-      
       def initialize(url: nil, path: nil)
         log "Downloading Lambda code from #{url}"
         case url
           when nil
-            @s3_url = URI(upload(make_filename(path.split('/').last), fetch_path(path).read, gzip: false))
+            @s3_url = if File.directory?(path)
+                        URI(upload(make_filename(path.split('/').last), fetch_from_folder(path), mime_type: 'application/zip',  gzip: false))
+                      else
+                        URI(upload(make_filename(path.split('/').last), File.open(path, "rb").read, gzip: false))
+                      end
           else
-            res = fetch_url(url)
+            res = fetch_from_url(url)
             @s3_url = URI(upload(make_filename(url.split('.').last), res.body, mime_type: res['content-type'], gzip: false))
           end
         log "uploaded Lambda function to #{@s3_url}"
       end
       
-      def fetch_path(path_str)
-        if File.directory?(path_str)
-          Zip::ZipFile.open(path_str.split('/').last, Zip::ZipFile::CREATE) do |zipfile|
-              Dir[File.join(path_str, '*')].each do |file|
-                zipfile.add(file.sub(path_str, ''), file)
-              end
+      def fetch_from_folder(path_str)
+        begin
+          temp_file = Tempfile.new("#{path_str.split('/').last}.zip")
+          Zip::ZipOutputStream.open(temp_file) { |zos| }
+          Zip::ZipFile.open(temp_file.path, Zip::ZipFile::CREATE) do |zipfile|
+            Dir[File.join(path_str, '*')].each do |file|
+              zipfile.add(file.sub("#{path_str}/", ''), file)
+            end
           end
+          zip_data = File.read(temp_file.path)
+        ensure
+          temp_file.close
+          temp_file.unlink
         end
-        File.open(path_str, "rb") #if its a zip - will it open the zip or the folder (by the same name)?
+        zip_data
       end
       
-      def fetch_url(uri_str, limit = 10)
+      def fetch_from_url(uri_str, limit = 10)
         raise ArgumentError, 'too many HTTP redirects' if limit == 0
         response = Net::HTTP.get_response(URI(uri_str))
         case response
