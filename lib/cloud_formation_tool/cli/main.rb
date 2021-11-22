bb57b15b62f40f370719f06f4dc05d73988ef147
Oded Arbel <oded@geek.co.il>
2021-11-22 08:00:58 +0200

    fixed typo

--- lib/cloud_formation_tool/cli/main.rb
+++ lib/cloud_formation_tool/cli/main.rb
@@ -55,7 +55,7 @@ module CloudFormationTool
       subcommand 'servers', 'List stack resources', Servers
       subcommand 'groups', 'List stack Autoscaling groups', Groups
       subcommand 'recycle', 'Recycle servers in an auto scaling group', Recycle
-      subcommand 'scale', 'Set the number of desired servesr in an auto scaling group', Scale
+      subcommand 'scale', 'Set the number of desired servers in an auto scaling group', Scale
       subcommand 'invalidate', 'Invalidate CloudFront caches in the stack', Invalidate
       subcommand 'output', 'Retrieve output values from the stack', Output
     end
